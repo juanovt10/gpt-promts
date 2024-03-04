e2536ee08428f0c84b0e6e373bd19b98e39ae87e
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-04 14:36:27 +0100

    Change next.config file to attempt deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["lh3.googleusercontent.com"],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-      return config;
-    },
-  };
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
 
-  
-  export default nextConfig;
+module.exports = nextConfig
