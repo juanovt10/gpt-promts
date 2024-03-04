71d656a8d9d9b32993df2e390583742e75493547
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-04 15:04:59 +0100

    amend next.config to attempt deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,15 +6,16 @@ const nextConfig = {
     missingSuspenseWithCSRBailout: false,
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ["lh3.googleusercontent.com"],
   },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    }
-    return config
-  }
-}
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+
+export default nextConfig;
