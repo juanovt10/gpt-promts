7045f885729082c34757f79cc3fd0b81effac951
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-04 15:13:27 +0100

    try again deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
 };
-
 
 export default nextConfig;
